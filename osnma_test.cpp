639316dfd6583acb4839765292abc3fc49115eff
guldur <guldur@7e91826a-1d71-0f41-93d9-fd6e3229cfe2>
2026-06-17 17:29:10 +0000

    +
    git-svn-id: https://bruine.org/svn/svnrepository/osnmalib@1085 7e91826a-1d71-0f41-93d9-fd6e3229cfe2

--- osnma_test.cpp
+++ osnma_test.cpp
@@ -12,7 +12,8 @@
 
 // -tv "D:\data\osnma\Test_vectors" > d:\data\osnma\out.txt
 // -tv "D:\data\osnma\Test_vectors" configuration_1 16_AUG_2023_GST_05_00_01.csv > d:\data\osnma\out.txt
-// -json D:\data\osnma\week_1397_24h.jsonl D:\data\osnma\OSNMA_MerkleTree_20251210100000_newPKID_2.xml D:\data\osnma\OSNMA_PublicKey_20251210100000_newPKID_2.xml
+// -json D:\data\osnma\week_1397_1h.jsonl D:\data\osnma\OSNMA_MerkleTree_20251210100000_newPKID_2.xml D:\data\osnma\OSNMA_PublicKey_20251210100000_newPKID_2.xml > d:\data\osnma\jsonout.txt
+// -json D:\data\osnma\week_1397_24h.jsonl D:\data\osnma\OSNMA_MerkleTree_20251210100000_newPKID_2.xml D:\data\osnma\OSNMA_PublicKey_20251210100000_newPKID_2.xml > d:\data\osnma\jsonout.txt
 
 static void PrintUsage(const char* exe_name)
 {
